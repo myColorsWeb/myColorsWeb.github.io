dd16fac943a31bf93bd7af2e22e919488368b628
Brian Jr <55070185+BrianJr03@users.noreply.github.com>
2022-09-29 14:03:35 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "97a6d6f187e236e0a124da7b2283fc96",
-"/": "97a6d6f187e236e0a124da7b2283fc96",
-"main.dart.js": "e550ecb345b7e9b5af69ab2e59ce48fb",
+"index.html": "b470ad26df3eb70e7750add2b999fec3",
+"/": "b470ad26df3eb70e7750add2b999fec3",
+"main.dart.js": "9bdfaf611da2e038cc1cbc4a76ae40a1",
 "manifest.json": "7dbcd59c6a147e4bfa3faac7948e3954",
 "version.json": "8b8d9f42c542d84df3cc52b28020f541"
 };
